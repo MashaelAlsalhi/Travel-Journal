6cca386cc0da93084d491e6bc6bca9e7e0afd039
unknown <meshael.a.alsalhi@gmail.com>
2022-02-14 11:48:03 +0300

    Second one

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Navbar from "./Components/Navbar";
-import Call_Cards from "./Components/call_Cards"
+import Call_cards from "./Components/call_Cards"
 import Footer from "./Components/footer"
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Call_Cards/>
+      <Call_cards/>
       <Footer/>
     </div>
   );
